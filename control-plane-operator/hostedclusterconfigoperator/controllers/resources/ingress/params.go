084613bcd4d1e583bdf075ad9056d6036d990fb7
Cesar Wong <cewong@redhat.com>
2022-06-14 18:19:26 -0400

    Improve user experience of HostedCluster configuration API
    Currently, global configuration of HostedClusters needs to be specified
    as an arbitrary list of RawExtensions. This forces users to have to
    properly specify kind and apiVersion on basic configuration resources.
    In addition to that, redundant information needs to be provided as a
    list of secret and configmap references.
    
    This change simplifies hosted cluster configuration by including
    specific fields for each configuration resource's spec. The user has
    now explicit fields where they can specify configuration for their
    cluster.

--- control-plane-operator/hostedclusterconfigoperator/controllers/resources/ingress/params.go
+++ control-plane-operator/hostedclusterconfigoperator/controllers/resources/ingress/params.go
@@ -11,13 +11,13 @@ type IngressParams struct {
 	PlatformType     hyperv1.PlatformType
 }
 
-func NewIngressParams(hcp *hyperv1.HostedControlPlane, globalConfig globalconfig.GlobalConfig) *IngressParams {
-	var replicas int32 = 2
-	if hcp.Spec.InfrastructureAvailabilityPolicy == hyperv1.SingleReplica {
-		replicas = 1
+func NewIngressParams(hcp *hyperv1.HostedControlPlane) *IngressParams {
+	var replicas int32 = 1
+	if hcp.Spec.InfrastructureAvailabilityPolicy == hyperv1.HighlyAvailable {
+		replicas = 2
 	}
 	return &IngressParams{
-		IngressSubdomain: globalconfig.IngressDomain(hcp, globalConfig.Ingress),
+		IngressSubdomain: globalconfig.IngressDomain(hcp),
 		Replicas:         replicas,
 		PlatformType:     hcp.Spec.Platform.Type,
 	}
